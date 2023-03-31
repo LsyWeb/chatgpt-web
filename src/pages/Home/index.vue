ac358d71a0474ee15c9cba3f57e6ee11bec76fe0
刘帅洋 <liushuaiyang@aliyun.com>
2023-03-31 16:31:23 +0800

    feat: laf云开发的appid替换为环境变量

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -38,10 +38,15 @@ import Search from '@/components/Search/index.vue';
 import BackTop from '@/components/BackTop/index.vue';
 highjs.highlightAll();
 marked.setOptions({
-  highlight: function (code) {
+  highlight: function (code, lang) {
+    if (lang && highjs.getLanguage(lang)) {
+      return highjs.highlight(code, { language: lang }).value;
+    }
     return highjs.highlightAuto(code).value;
   },
 });
+
+const APP_ID = import.meta.env.VITE_LAF_APP_ID;
 
 const state = reactive({
   loading: false,
@@ -50,15 +55,17 @@ const state = reactive({
 });
 const isChat = computed(() => state.messageList.length > 0); // 是否开始聊天
 
-const parentMessageId = ref(''); // 上下文消息id
+const parentMessageId = ref(); // 上下文消息id
 
 const scrollRef = ref<HTMLElement | null>(null); // 滚动容器
 const observer = ref<MutationObserver | null>(null); // 监听滚动容器的clientHeight变化
 
-// 监听滚动到底部
+// 监听滚动，在chatgpt输出文字的时候，自动滚动到底部
 watch([isChat, scrollRef], () => {
   let falg = false;
   if (isChat.value && scrollRef.value!) {
+    // 这里使用 MutationObserver 来监听元素属性的变化，
+    // MutationObserver Api参考: https://developer.mozilla.org/zh-CN/docs/Web/API/MutationObserver
     observer.value = new MutationObserver((entries) => {
       if (!falg) {
         requestAnimationFrame(() => {
@@ -78,10 +85,11 @@ watch([isChat, scrollRef], () => {
 });
 
 let flag = false;
-// 滚动到底部
+// 滚动事件
 const onScroll = (e: any) => {
   if (!flag) {
     requestAnimationFrame(() => {
+      // 控制回到顶部按钮的显示/隐藏
       if (e?.target?.scrollTop > 100) {
         state.isShowBackTop = true;
       } else {
@@ -103,6 +111,7 @@ const sendMessage = async (searchValue: string) => {
     return;
   }
 
+  // 添加用户输入的消息
   state.messageList.push({
     id: v4(),
     type: MessageType.User,
@@ -120,7 +129,6 @@ const sendMessage = async (searchValue: string) => {
 
   let loadingTyped: Typed | null = null;
   nextTick(() => {
-    // console.log(document.querySelector(`#loading-id-${activeId}`));
     loadingTyped = new Typed(`#loading-id-${activeId}`, {
       strings: ['', '加载中...'], //输入内容, 支持html标签
       typeSpeed: 50,
@@ -128,60 +136,49 @@ const sendMessage = async (searchValue: string) => {
   });
 
   const cloud = new Cloud({
-    baseUrl: 'https://f6x6ul.laf.dev',
+    baseUrl: `https://${APP_ID}.laf.dev`,
     getAccessToken: () => '',
-    timeout: 180000,
+    timeout: 180000, // 由于一些问题可能会导致chatgpt思考很长时间，所以接口的超时时长要设长一些
   });
 
-  let res;
   state.loading = true;
-  // 与云函数逻辑一样，有上下文 id 就传入
-  if (!parentMessageId.value) {
-    res = await cloud
-      .invoke('sendMessage', { message: searchValue })
-      .catch((err) => {
-        state.loading = false;
-        state.messageList = state.messageList.map((item) => {
-          if (item.id === activeId) {
-            return {
-              ...item,
-              loading: false,
-              error: {
-                msg: err,
-              },
-            };
-          }
-          return item;
-        });
-        return;
+  // 发送消息
+  const res = await cloud
+    .invoke('sendMessage', {
+      message: searchValue,
+      parentMessageId: parentMessageId.value,
+    })
+    .catch((err) => {
+      state.loading = false;
+      state.messageList = state.messageList.map((item) => {
+        if (item.id === activeId) {
+          return {
+            ...item,
+            loading: false,
+            error: {
+              msg: err,
+            },
+          };
+        }
+        return item;
       });
-  } else {
-    res = await cloud
-      .invoke('sendMessage', {
-        message: searchValue,
-        parentMessageId: parentMessageId.value,
-      })
-      .catch((err) => {
-        state.loading = false;
-        state.messageList = state.messageList.map((item) => {
-          if (item.id === activeId) {
-            return {
-              ...item,
-              loading: false,
-              error: {
-                msg: err,
-              },
-            };
-          }
-          return item;
-        });
-        return;
-      });
-  }
+      return;
+    });
 
   parentMessageId.value = res.id;
   const content = res.text;
 
+  state.messageList = state.messageList.map((item) => {
+    if (item.id === activeId) {
+      return {
+        ...item,
+        loading: false,
+        content,
+      };
+    }
+    return item;
+  });
+  onScrollEnd();
   nextTick(() => {
     new Typed(`#text-id-${activeId}`, {
       strings: ['', marked(content)], //输入内容, 支持html标签
@@ -194,16 +191,6 @@ const sendMessage = async (searchValue: string) => {
     });
   });
   (loadingTyped as unknown as Typed)?.destroy(); // 销毁loading文字
-  state.messageList = state.messageList.map((item) => {
-    if (item.id === activeId) {
-      return {
-        ...item,
-        loading: false,
-        content,
-      };
-    }
-    return item;
-  });
 };
 
 const onScrollEnd = () => {
